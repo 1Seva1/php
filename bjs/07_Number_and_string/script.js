d96596527a41eb4538eb3abd2728ec2fc6eac33e
1Seva1 <sevostyanovmv@gmail.com>
2022-02-08 16:32:40 +0300

    edit index&script task 7.7

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,10 +1,40 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#inputWindow');
 
+document.querySelector('#btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
 
-document.getElementById('btn_clr').addEventListener('click', function () {
+document.querySelector('#btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.querySelector('#btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+})
+
+document.querySelector('#btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+document.querySelector('#btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
